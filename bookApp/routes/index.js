00a565453b3e8ada1376876bec47a2912a87d658
reetipd <reetipd@gmail.com>
2020-10-31 14:11:44 +0545

    Folder added

--- bookApp/routes/index.js
+++ bookApp/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/reety', function(req, res, next) {
+  res.render('index', { title: 'reety' });
+});
 
 module.exports = router;
