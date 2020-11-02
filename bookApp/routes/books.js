2f82063e4899889668555a4f59148f657c68a82d
reetipd <reetipd@gmail.com>
2020-11-02 18:13:40 +0545

    Edit-Book

--- bookApp/routes/books.js
+++ bookApp/routes/books.js
@@ -1,6 +1,5 @@
 
 var express = require('express');
-const { concat } = require('../resources/books');
 var books = require('../resources/books');
 var router = express.Router();
 
@@ -21,9 +20,14 @@ router.post('/save', function (req, res, next) {
 
 //new router for deleting book
 router.get('/remove/:index',function (req,res){
-    books.splice(req.params.index, 1); //(,delete count)
+    console.log(req.params.index);
+    books.splice(req.params.index, 1); //(books array ko index,delete count)
     res.redirect('/');
 });
+
+// router.get('/remove/:_id',function(req,res){
+//     console.log(req.params._id);
+// }) //:_id - book wala id -001,002...
 
 
 router.get('/edit/:_id', function(req,res) {
@@ -38,18 +42,19 @@ router.get('/edit/:_id', function(req,res) {
 router.post('/edit/:_id/editsave', function (req, res, next) {
     console.log('In editsave function...', req.body);
     //const editBook = req.body;
-    // console.log(req.params._id)
-    // books.forEach((book) => {
-    //     if(book._id === req.params._id){
-    //         console.log(book._id);
-    //         //console.log(req.body.title)
-    //         book.title = req.body.title;
-    //         book.author = req.body.author;
-    //         book.description = req.body.description;
-    //         book.genre = req.body.genre;
-    //     }
-    // })
-    books.splice(req.params._id-1, 1, req.body);
+    console.log(req.params._id)
+    books.forEach((book) => {
+        if(book._id === req.params._id){
+            console.log(book._id);
+            //console.log(req.body.title)
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.description = req.body.description;
+            book.genre = req.body.genre;
+        }
+    })
+
+    books.splice(req.params._id-1, 1, req.body); //after deleting kunai book index ma affect garira cha
 
     
     res.redirect('/');
